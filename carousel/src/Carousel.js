51b2ac2d916b2018b98b00135210dc07563b95a6
Andy <jihyuk.sung@gmail.com>
2022-01-03 22:45:48 -0700

    Finished

--- carousel/src/Carousel.js
+++ carousel/src/Carousel.js
@@ -1,39 +1,55 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./Carousel.css";
 import image1 from "./image1.jpg";
 import image2 from "./image2.jpg";
 import image3 from "./image3.jpg";
 import Card from "./Card";
 
-function Carousel(props) {
-  const [cardIdx, setCardIdx] = useState(0);
-  const card = props.cardData[cardIdx];
-  const total = props.cardData.length;
-  const goForward = () => setCardIdx(cardIdx + 1);
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardIdx: 0 };
+    this.goForward = this.goForward.bind(this);
+    this.goBack = this.goBack.bind(this);
+  }
 
-  return (
-    <div className="Carousel">
-      <h1>{props.title}</h1>
-      <div className="Carousel-main">
-        <i
-          className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
-          data-testid="left-arrow"
-        />
-        <Card
-          caption={card.caption}
-          src={card.src}
-          currNum={cardIdx + 1}
-          totalNum={total}
-        />
-        <i
-          className="fas fa-chevron-circle-right fa-2x"
-          onClick={goForward}
-          data-testid="right-arrow"
-        />
+  goForward() {
+    this.setState(st => ({ cardIdx: st.cardIdx + 1 }));
+  }
+
+  goBack() {
+    this.setState(st => ({ cardIdx: st.cardIdx - 1 }));
+  }
+
+  render() {
+    const card = this.props.cardData[this.state.cardIdx];
+    const total = this.props.cardData.length;
+    const leftIconHidden = this.state.cardIdx === 0 ? "hidden" : "";
+    const rightIconHidden = this.state.cardIdx === total - 1 ? "hidden" : "";
+    return (
+      <div className="Carousel">
+        <h1>{this.props.title}</h1>
+        <div className="Carousel-main">
+          <i
+            className={`fas fa-chevron-circle-left fa-2x ${leftIconHidden}`}
+            onClick={this.goBack}
+            data-testid="left-arrow"
+          />
+          <Card
+            caption={card.caption}
+            src={card.src}
+            currNum={this.state.cardIdx + 1}
+            totalNum={total}
+          />
+          <i
+            className={`fas fa-chevron-circle-right fa-2x ${rightIconHidden}`}
+            onClick={this.goForward}
+            data-testid="right-arrow"
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Carousel.defaultProps = {
